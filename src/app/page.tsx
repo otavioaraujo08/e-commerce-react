9174785fe49a4bc53807a018a9cb7c47dc5825de
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-07-04 22:04:59 -0300

    feat: add how much customers have bought the product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,6 +78,11 @@ export default function Home() {
                   <Star className="h-4 w-4 text-green-600 fill-green-600" />
                   <Star className="h-4 w-4 text-green-600 fill-green-600" />
                 </div>
+
+                <p>
+                  <span className="font-semibold">1.250</span> compradores
+                  felizes
+                </p>
               </div>
             </div>
           </div>
